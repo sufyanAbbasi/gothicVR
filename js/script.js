46243a84e9ccc7cf28965cdfb071c005f4fb1824
sufyanAbbasi <home@Amylab-1.local>
2016-04-05 12:12:58 -0400

    Stereoscope with Zoomify

--- js/script.js
+++ js/script.js
@@ -1,89 +1,41 @@
-
-var leftImgArr = ["./stereo_images/Albi_074.png"];
-var rightImgArr = ["./stereo_images/Albi_075.png"];
-
-var getPointerEvent = function(event) {
-	    return event.originalEvent.targetTouches ? event.originalEvent.targetTouches[0] : event;
-	};
-	
-var $touchArea = $('#touchArea'),
-    touchStarted = false, // detect if a touch event is sarted
-    currX = 0,
-    currY = 0,
-    cachedX = 0,
-    cachedY = 0;
-    gyroModeOn = false;
-
-function gyroMode(){
-	if(!gyroMode){
-
-		//code to start gyro
-		
-		gyroMode = true;
-	}else{
-		//code to stop gyro
-
-		gyroMode = false;
-	}
-}
-
-
 function init(){
 
-	$("#left_image").attr("src", leftImgArr[0]);
-	$("#right_image").attr("src", rightImgArr[0]);
+    gyro.frequency = 100;
 
-	gyro.frequency = 100;
+    var leftFrame = document.getElementById("left_image").contentWindow;
+    var rightFrame = document.getElementById("right_image").contentWindow;
 
 	gyro.startTracking(function(o) {
 	        // o.x, o.y, o.z for accelerometer
 	        // o.alpha, o.beta, o.gamma for gyro
 
+	        //gamma - while in landscape view:
+	        //        flat with screen facing down is 0°, and rotating towards being flat to your face increases to 90° 
+	        //        then becomes -89.999 --> -0 
+
 
 	        $('#values').html("x: " + o.x + "<br>y: " + o.y + "<br>z: " + o.z + 
 	        	               "<br>alpa: " + o.alpha + "<br>beta: " + o.beta + "<br>gamma: " + o.gamma);
 
+			if(o.gamma > 0 && o.gamma < 85){
+				//tilting up in landscape
+				leftFrame.$('document').trigger('tiltup');
+				rightFrame.$('document').trigger('tiltup');
+				
 
-
-
+			}else if(o.gamma > -85 && o.gamma < 0){
+				//tilting down in landscape
+				//leftFrame.$('document').trigger('tiltdown');
+				//rightFrame.$('document').trigger('tiltdown');
+				
+			}else{
+				//leftFrame.$('document').trigger('notilt');
+				//rightFrame.$('document').trigger('notilt');
+			}
 	    });
-
-	//setting the events listeners
-	$touchArea.on('touchstart mousedown',function (e){
-	    e.preventDefault(); 
-	    var pointer = getPointerEvent(e);
-	    // caching the current x
-	    cachedX = currX = pointer.pageX;
-	    // caching the current y
-	    cachedY = currY = pointer.pageY;
-	    // a touch event is detected      
-	    touchStarted = true;
-	    // detecting if after 200ms the finger is still in the same position
-	    setTimeout(function (){
-	        if ((cachedX === currX) && !touchStarted && (cachedY === currY)) {
-	            // Here you get the Tap event
-	            gyroMode();
-	        }
-	    },200);
-
-	});
-	$touchArea.on('touchend mouseup touchcancel',function (e){
-	    e.preventDefault();
-	    // here we can consider finished the touch event
-	    touchStarted = false;
-	});
-
-	$touchArea.on('touchmove mousemove',function (e){
-	    e.preventDefault();
-	    var pointer = getPointerEvent(e);
-	    currX = pointer.pageX;
-	    currY = pointer.pageY;
-	    if(touchStarted) {
-	         // here you are swiping
-	    }
-	   
-	});
 	}
+
+
 
 $(init());
 
