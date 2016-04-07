401129a191319d0ed5f189ad0df10fe5c8dac36e
sufyanAbbasi <home@Amylab-1.local>
2016-04-07 16:46:09 -0400

    Zoomers now work!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+function dispatchToIframes(eventString){
+	$('#left_image')[0].contentWindow.document.dispatchEvent(new Event(eventString));
+	$('#right_image')[0].contentWindow.document.dispatchEvent(new Event(eventString));	
+}
+
 function init(){
 
     gyro.frequency = 100;
@@ -17,52 +22,85 @@ function init(){
 
 			if(o.gamma > 0 && o.gamma < 85){
 				//tilting up in landscape
-				var tiltUp = new Event('tilt-up');
-				$('#left_image')[0].contentWindow.document.dispatchEvent(tiltUp);
-				$('#right_image')[0].contentWindow.document.dispatchEvent(tiltUp);
+				dispatchToIframes('tilt-up');
 				
 
 			}else if(o.gamma > -75 && o.gamma < 0){
 				//tilting down in landscape
-				var tiltDown = new Event('tilt-down');
-				$('#left_image')[0].contentWindow.document.dispatchEvent(tiltDown);
-				$('#right_image')[0].contentWindow.document.dispatchEvent(tiltDown);
+				dispatchToIframes('tilt-down');
 				
 			}else{
-				var noTilt = new Event('no-vertical-tilt');
-				$('#left_image')[0].contentWindow.document.dispatchEvent(noTilt);
-				$('#right_image')[0].contentWindow.document.dispatchEvent(noTilt);
+				dispatchToIframes('no-vertical-tilt');
 			}
 
-			if(o.beta > 3){
-				//tilting up in landscape
-				var tiltLeft = new Event('tilt-left');
-				$('#left_image')[0].contentWindow.document.dispatchEvent(tiltLeft);
-				$('#right_image')[0].contentWindow.document.dispatchEvent(tiltLeft);
-				
+			if(o.gamma < 0){
 
-			}else if(o.beta < -3){
-				//tilting down in landscape
-				var tiltRight = new Event('tilt-right');
-				$('#left_image')[0].contentWindow.document.dispatchEvent(tiltRight);
-				$('#right_image')[0].contentWindow.document.dispatchEvent(tiltRight);
-				
+				if(o.beta > 2.5){
+					//tilting up in landscape
+					dispatchToIframes('tilt-left');
+					
+
+				}else if(o.beta < -2.5){
+					//tilting down in landscape
+					dispatchToIframes('tilt-right');
+					
+				}else{
+					dispatchToIframes('no-horizontal-tilt');
+				}
 			}else{
-				var noTilt = new Event('no-horizontal-tilt');
-				$('#left_image')[0].contentWindow.document.dispatchEvent(noTilt);
-				$('#right_image')[0].contentWindow.document.dispatchEvent(noTilt);
-			}
+				// if(o.beta < 0 && o.beta > -175){
+				// 	//tilting up in landscape
+				// 	dispatchToIframes('tilt-left');
+					
 
+				// }else if(o.beta > ){
+				// 	//tilting down in landscape
+				// 	dispatchToIframes('tilt-right');
+					
+				// }else{
+				// 	dispatchToIframes('no-horizontal-tilt');
+				// }
+			}
 	    });
 	}
 
-	var hammerTime = new Hammer(document);
-	hammerTime.on('tap', function(e){
-		e.preventDefault();
-		var tap = new Event('tap');
-		$('#left_image')[0].contentWindow.document.dispatchEvent(tap);
-		$('#right_image')[0].contentWindow.document.dispatchEvent(tap);
+	//$('.values').html("Listening for events.");
+
+	var eventListener = new Hammer(document);
+	
+	eventListener.get('press').set({
+		time: 1000,
+		threshold: 30,
 	});
+
+	eventListener.get('tap').set({
+		threshold: 700,
+	});
+
+	eventListener.get('press').dropRecognizeWith(eventListener.get('tap'));
+
+	eventListener.on('press', function(ev){
+		//$('.values').html("Press event fired.");
+		dispatchToIframes('zoom-start');
+
+	});
+
+	eventListener.on('pressup', function(ev){
+		//$('.values').html("Pressup event fired.");
+		// setTimeout(function(){
+		// 	$('.values').html("Listening for events.");
+		// }, 500);
+		dispatchToIframes('zoom-end');
+	});
+
+	eventListener.on('tap', function(ev){
+		// $('.values').html("Tap event fired");
+		// setTimeout(function(){
+		// 	$('.values').html("Listening for events.");
+		// }, 500);
+
+		dispatchToIframes('zoom-out');
+	})
 
 $(init());
 
